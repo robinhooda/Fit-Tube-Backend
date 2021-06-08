2934022530ccc0f24fe7218dc36e6d81b9787480
robinhooda <robinhooda66@gmail.com>
2021-06-09 03:27:49 +0530

    Add. timestamps to user Model

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,31 @@
 const { Schema, model } = require('mongoose')
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: 'Please enter valid name to continue!',
-    minlength: 4,
-    maxlength: 255,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: 'Please enter valid name to continue!',
+      minlength: 4,
+      maxlength: 255,
+    },
+    email: {
+      type: String,
+      required: 'Please enter valid email to continue!',
+      minlength: 6,
+      maxlength: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: 'Please enter valid password to continue!',
+      min: 6,
+      max: 255,
+    },
   },
-  email: {
-    type: String,
-    required: 'Please enter valid email to continue!',
-    minlength: 6,
-    maxlength: 255,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: 'Please enter valid password to continue!',
-    min: 6,
-    max: 255,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 const User = model('User', userSchema)
 module.exports = User
